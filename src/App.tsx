f2a4a80f5dc594a381620e3fba51ef0d285c3279
Arani Sen <aranisen@Aranis-MBP.attlocal.net>
2023-10-05 13:48:09 -0500

    init WIP

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import MetaMask from "./components/MetaMask";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <MetaMask />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MetaMask.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+import { hooks, metaMask } from "../connectors/metaMaskHook";
+
+const {
+  useChainId,
+  useAccounts,
+  useIsActivating,
+  useIsActive,
+  useProvider,
+  useENSNames,
+} = hooks;
+
+export default function MetaMask() {
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const isActivating = useIsActivating();
+
+  const isActive = useIsActive();
+
+  const provider = useProvider();
+  const ENSNames = useENSNames(provider);
+
+  const [error, setError] = useState(undefined);
+
+  // attempt to connect eagerly on mount
+  useEffect(() => {
+    void metaMask.connectEagerly().catch(() => {
+      console.debug("Failed to connect eagerly to metamask");
+    });
+  }, []);
+
+  return (
+    <div>
+      <b>{getName(connector)}</b>
+      <div style={{ marginBottom: "1rem" }}>
+        <Status isActivating={isActivating} isActive={isActive} error={error} />
+      </div>
+      <Chain chainId={activeChainId} />
+      <div style={{ marginBottom: "1rem" }}>
+        <Accounts accounts={accounts} provider={provider} ENSNames={ENSNames} />
+      </div>
+      <ConnectWithSelect
+        connector={connector}
+        activeChainId={activeChainId}
+        chainIds={chainIds}
+        isActivating={isActivating}
+        isActive={isActive}
+        error={error}
+        setError={setError}
+      />
+    </div>
+  );
+}
